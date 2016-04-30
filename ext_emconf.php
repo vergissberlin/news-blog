7c570ff05197bb02d582c8d13bcba5859e8da217
Pascal Dürsteler <pascal@nimius.net>
2016-04-30 17:49:53 +0200

    TASK | Update dependencies, allow tx_news > 4.0.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,9 +22,8 @@ $EM_CONF[$_EXTKEY] = array (
 	'author_email' => 'info@nimius.net',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.4.0-5.6.99',
 			'typo3' => '6.2.0-7.6.99',
-			'news' => '3.2.0-4.0.99'
+			'news' => '3.2.0-4.9.99'
 		),
 	),
 );
